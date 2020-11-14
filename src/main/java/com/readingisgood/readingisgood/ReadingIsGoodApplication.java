c9ea45a3724a97ccf3ef240ec4481dbe246e9f54
Eren Adıgüzel <erenadiguzellll@gmail.com>
2020-11-14 22:33:44 +0300

    Project created.

---
+++ src/main/java/com/readingisgood/readingisgood/ReadingIsGoodApplication.java
@@ -0,0 +1,13 @@
+package com.readingisgood.readingisgood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadingIsGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadingIsGoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/readingisgood/readingisgood/ReadingIsGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.readingisgood.readingisgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingIsGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
